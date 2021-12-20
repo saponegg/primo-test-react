f862f10ea2431b498af0b34abde9ecc02fa83bc9
saponegg <saponegg@gmail.com>
2021-12-20 17:24:23 +0100

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
-
+import Primo from "./Primo";
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <Primo/> 
     </div>
+
   );
 }
---
+++ src/Primo.js
@@ -0,0 +1,11 @@
+import React from "react";
+//i moduli devono avere sempre la lettera grande
+const Primo = () => {
+  return <div> ciao questa è un component </div>; //deve sempre ritornare qualcosa
+
+  
+
+
+};
+
+export default Primo;
