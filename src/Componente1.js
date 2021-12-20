2ad686a023f5e2afde8762cb1d802eb414502b75
saponegg <saponegg@gmail.com>
2021-12-20 18:00:39 +0100

    1 file modified

--- src/Componente1.js
+++ src/Componente1.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+//qui ho un return multiplo di elementi racchiusi in un div
+// oppure uso React.Fragment per racchiudere tutto e considerarli elementi separati
+//ClassName serve per indicare le classi di stile ovvero il vecchio class
 const Componente1 = () => {
   return (
+    <React.Fragment>
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1 ClassName="prova">Componente 1</h1>
+      <p>Questo componente racchiude 2 elementi e per questo li ho racchiuysi in una div </p>
     </div>
-
+    </React.Fragment>
   );
 };
 
