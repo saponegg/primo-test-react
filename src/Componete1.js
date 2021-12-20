fb882f8584e759e76516f4e79649c21a6bbf3f89
saponegg <saponegg@gmail.com>
2021-12-20 17:36:06 +0100

    2 files modified, 1 added

---
+++ src/Componete1.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Componente1 = () => {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+
+  );
+};
+
+export default Componente1;
--- src/Primo.js
+++ src/Primo.js
@@ -1,7 +1,7 @@
 import React from "react";
-//i moduli devono avere sempre la lettera grande
+//i compomnenti devono avere sempre la lettera grande
 const Primo = () => {
-  return <div> ciao questa è un component </div>; //deve sempre ritornare qualcosa
+  return <div> ciao questa è un component </div>; //deve sempre ritornare qualcosa e questa cosa deve essere unica
 
   
 
