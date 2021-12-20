80e3247cf0dbabcae73b9916338c717f93d50508
saponegg <saponegg@gmail.com>
2021-12-20 17:37:20 +0100

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./style.css";
 import Primo from "./Primo";
-import Componente1 from"./Componente1";
+import Componente1 from "./Componente1";
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
       <Primo/> 
-      <Componente1>
+      <Componente1/>
     </div>
 
   );
